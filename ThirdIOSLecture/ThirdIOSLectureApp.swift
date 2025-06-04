bc7e513276d1f3c7e2853921378ba4a7a7970235
bizhanchik <bizhanash@gmail.com>
2025-06-04 14:16:04 +0500

    Initial Commit

---
+++ ThirdIOSLecture/ThirdIOSLectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdIOSLectureApp.swift
+//  ThirdIOSLecture
+//
+//  Created by Bizhan on 04.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ThirdIOSLectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
